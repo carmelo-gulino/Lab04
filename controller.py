377abeac841c08aec225fdcd5e25c6e56924b94b
Carmelo Gulino <s298109@studenti.polito.it>
2024-03-26 18:01:58 +0100

    user interfafcecedcd

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import time
 import flet as ft
 import model as md
 
+
 class SpellChecker:
 
     def __init__(self, view):
@@ -44,10 +45,9 @@ class SpellChecker:
             case _:
                 return None
 
-
     def printMenu(self):
         print("______________________________\n" +
-              "      SpellChecker 101\n"+
+              "      SpellChecker 101\n" +
               "______________________________\n " +
               "Seleziona la lingua desiderata\n"
               "1. Italiano\n" +
@@ -56,6 +56,21 @@ class SpellChecker:
               "4. Exit\n" +
               "______________________________\n")
 
+    def handleSpellCheck(self, e):
+        if (self._view._searchType.value is None) or (self._view._lang.value is None) or (self._view._txtIn.value == ""):
+            self._view.handleMissingFields()
+        else:
+            output = self.handleSentence(self._view._txtIn.value, self._view._lang.value, self._view._searchType.value)
+            self._view.printWrong(self._view._txtIn.value, output)
+        self._view.cleanFields()
+
+    def selectLanguage(self, e):
+        if not (self._view._lang.value is None):
+            self._view.confirmLang()
+
+    def selectType(self, e):
+        if not (self._view._searchType.value is None):
+            self._view.confirmType()
 
 def replaceChars(text):
     chars = "\\`*_{}[]()>#+-.!$?%^;,=_~"
--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 import flet as ft
+from time import sleep
 
 class View(object):
     def __init__(self, page: ft.Page):
@@ -27,15 +28,71 @@ class View(object):
         )
 
         # Add your stuff here
+        #Row1
+        self._lang = ft.Dropdown(label="Select language", width=450, on_change=self.__controller.selectLanguage)
+        self.fillLang()
+        row1 = ft.Row([self._lang])
 
-        self.page.add([])
+        #Row2
+        self._searchType = ft.Dropdown(label="Search type", width=150, on_change=self.__controller.selectType)
+        self.fillSearch()
+        self._txtIn = ft.TextField(label="Add your sentence here", width=750)
+        self._SpChBtn = ft.ElevatedButton(text="Spell Check", on_click=self.__controller.handleSpellCheck)
+        row2 = ft.Row([self._searchType, self._txtIn, self._SpChBtn])
 
+        self.page.add(row1, row2)
+        self.page.update()
+
+    def fillLang(self):
+        langs = ["italian", "english", "spanish"]
+        for lang in langs:
+            self._lang.options.append(ft.dropdown.Option(lang))
+        return self._lang
+
+    def fillSearch(self):
+        types = ["Default", "Linear", "Dichotomic"]
+        for type in types:
+            self._searchType.options.append(ft.dropdown.Option(type))
+        return self._searchType
+
+    def confirmLang(self):
+        self._msg = ft.ListView()
+        self._msg.controls.append(ft.Text("Language confirmed", color="blue"))
+        self.page.add(self._msg)
+        self.page.update()
+        self.sleep()
+
+    def confirmType(self):
+        self._msg = ft.ListView()
+        self._msg.controls.append(ft.Text("Search type confirmed", color="blue"))
+        self.page.add(self._msg)
+        self.page.update()
+        self.sleep()
+
+    def handleMissingFields(self):
+        self._errorMsg = ft.ListView()
+        self._errorMsg.controls.append(ft.Text("All the fields are required", color="red", size=20))
+        self.page.add(self._errorMsg)
+        self.page.update()
+        self.sleep()
+
+    def sleep(self):
+        sleep(0.5)
+        self.page.add()
         self.page.update()
 
     def update(self):
         self.page.update()
+
     def setController(self, controller):
         self.__controller = controller
+
+    def cleanFields(self):
+        self._searchType.value = None
+        self._lang.value = None
+        self._txtIn.value = None
+        self.update()
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
@@ -50,4 +107,12 @@ class View(object):
         # self.__txt_container.bgcolor = (
         #     ft.colors.GREY_900 if self.page.theme_mode == ft.ThemeMode.DARK else ft.colors.GREY_300
         # )
+        self.page.update()
+
+    def printWrong(self, txt, output):
+        self._msg = ft.ListView()
+        self._msg.controls.append(ft.Text(f"Frase inserita: {txt}\n"
+                                          f"Parole errate: - {output[0]}\n"
+                                          f"Tempo richiesto dalla ricerca: {output[1]}"))
+        self.page.add(self._msg)
         self.page.update()
